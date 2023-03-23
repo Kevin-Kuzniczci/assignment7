11e85bb76e5c0f65b2c14effdf783b1e4bc6efba
Kevin-Kuzniczci <60890825+Kevin-Kuzniczci@users.noreply.github.com>
2023-03-22 18:31:57 -0700

    ¯\_(ツ)_/¯ Followed the modules, still unusuable

--- js/script.js
+++ js/script.js
@@ -17,10 +17,10 @@ form.addEventListener('submit', (e) => {
     // INSERT A NEW ROW AT THE END OF THE EMPLOYEES TABLE
     let row = table.insertRow(-1);
     // INSERT A CELL FOR EACH ITEM WITHIN THE NEW ROW
-    let cellID = row.insertCell(newID);
-    let cellName = row.insertCell(newName);
-    let cellExtension = row.insertCell(newExtension);
-    let cellDepartment = row.insertCell(newDepartment);
+    let cellID = row.insertCell(0);
+    let cellName = row.insertCell(0);
+    let cellExtension = row.insertCell(0);
+    let cellDepartment = row.insertCell(0);
     // APPEND THE TEXT VALUES AS TEXT NODES WITHIN THE CELLS
     let textID = document.createTextNode(cellID);
     let textName = document.createTextNode(cellName);
@@ -31,20 +31,30 @@ form.addEventListener('submit', (e) => {
     row.appendChild(textName);
     row.appendChild(textExtension);
     row.appendChild(textDepartment);
+
+    table.appendChild(row);
     // CREATE THE DELETE BUTTON
     let textDelete = document.createElement('button');
     let visualTextDelete = document.createTextNode('X');
     textDelete.appendChild(visualTextDelete);
-    li.appendChild(textDelete);
+    row.appendChild(textDelete);
     // RESET THE FORM
     form.reset();
     // SET FOCUS BACK TO THE ID TEXT BOX
-    newID.focus();
+    // newID.focus();
     // INCREMENENT THE NUMBER OF EMPLOYEES IN THE TABLE
-    btncounter.addEventListener('click', function() {
+    btncounter.addEventListener('click', function countRows() {
         counter++;
         appendChild(counter);
     })
 })
 
 // DELETE EMPLOYEE
+
+table.addEventListener('click', (e) => {
+    if (e.target.classTable.contains('delete')) {
+        if(confirm(`Are you sure you want to delete the ${e.target.parentElement.innerText} task?`)) {
+            table.removeChild(e.target.parentElement);
+        }
+    }
+});
